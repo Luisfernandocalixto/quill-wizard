50f953da03a358e9c1f3e0f3f71c992845587775
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-07-21 15:57:57 -0600

    Add next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,  
+    },
+    
+};
+// consider for variables .env .env.local  
 
 export default nextConfig;
